ded33f35a9f61358ad107188f2dab9dfe9f002de
KseniaLichnova <Lichnova1@mail.ru>
2023-05-25 03:39:14 +0200

    init

--- index.js
+++ index.js
@@ -60,9 +60,9 @@ app.post('web-data', async (req, res) => {
             input_message_content: { message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму + totalPrice}`
             }
         })
-        return res.status(200).json({});
+        // return res.status(200).json({});
     } catch (e) {
-        return res.status(500).json({})
+        // return res.status(500).json({})
     }
 })
 
